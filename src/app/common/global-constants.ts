5c52aea16132b7c64d8eec3cc09baa16768cb055
Mario Flores <floresmario96@hotmail.com>
2020-05-28 21:22:11 -0500

    Tablas listas

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,17 +1,11 @@
 export class GlobalConstants {
-<<<<<<< HEAD
+
     public static serviciosURL: string = 'http://localhost:8080/';
     // public static serviciosURL: string = 'http://localhost:8080/BackEndAmelic-0.0.1-SNAPSHOT/';
     // public static url: string = 'http://localhost/ConocimientoAncestral/';
     //produccion
     //public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
-=======
-    //public static serviciosURL: string = 'http://localhost:8080/';
-    public static serviciosURL: string = 'http://localhost:8080/BackEndAmelic-0.0.1-SNAPSHOT/';
-    // public static url: string = 'http://localhost/ConocimientoAncestral/';
-    //produccion
-    // public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
->>>>>>> 6c58e1620a1fdc93cd7c84c9e430eff6e6cda0d3
+    
     public static url: string = 'http://amelica.org/ConocimientoAncestral/';
     public static siteTitle: string = 'Conocimineto Ancestral';
 }
