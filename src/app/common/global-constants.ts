4f2c73f0adc1da4199b3ea857b8100984511784d
Luis Angel Angeles <angel_apineda@hotmail.com>
2020-06-09 17:21:30 -0500

    busqueda pais

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,20 +1,10 @@
 export class GlobalConstants {
 
-<<<<<<< HEAD
     //public static serviciosURL: string = 'http://localhost:8080/';
-    public static serviciosURL: string = 'http://localhost:8080/BackEndAmelic-0.0.1-SNAPSHOT/';
-     public static url: string = 'http://localhost:4200/';
-    //produccion
-  //  public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
-   // public static url: string = 'http://amelica.org/ConocimientoAncestral/';
-    public static siteTitle: string = 'Conocimineto Ancestral';
-=======
-    // public static serviciosURL: string = 'http://localhost:8080/';
     // public static serviciosURL: string = 'http://localhost:8080/BackEndAmelic-0.0.1-SNAPSHOT/';
-    // public static url: string = 'http://localhost:4200/';
+    //  public static url: string = 'http://localhost:4200/';
     //produccion
-    public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
-    public static url: string = 'http://amelica.org/ConocimientoAncestral/';
-    // public static siteTitle: string = 'Conocimineto Ancestral';
->>>>>>> cc3e456a7faa8856466cc44c6ca05e54b096af98
+   public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
+   public static url: string = 'http://amelica.org/ConocimientoAncestral/';
+    public static siteTitle: string = 'Conocimineto Ancestral';
 }
