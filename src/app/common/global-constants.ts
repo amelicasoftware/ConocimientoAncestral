3d4e490a955d9e0d8055fc518144d72cd8d45fa7
Daniel Josafat Garcia <yrinengan@gmail.com>
2020-06-09 15:52:25 -0500

    errores corregidos

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,6 +1,5 @@
 export class GlobalConstants {
 
-<<<<<<< HEAD
     //public static serviciosURL: string = 'http://localhost:8080/';
     public static serviciosURL: string = 'http://localhost:8080/BackEndAmelic-0.0.1-SNAPSHOT/';
      public static url: string = 'http://localhost:4200/';
@@ -8,13 +7,7 @@ export class GlobalConstants {
   //  public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
    // public static url: string = 'http://amelica.org/ConocimientoAncestral/';
     public static siteTitle: string = 'Conocimineto Ancestral';
-=======
-    // public static serviciosURL: string = 'http://localhost:8080/';
-    // public static serviciosURL: string = 'http://localhost:8080/BackEndAmelic-0.0.1-SNAPSHOT/';
-    // public static url: string = 'http://localhost:4200/';
-    //produccion
-    public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
-    public static url: string = 'http://amelica.org/ConocimientoAncestral/';
-    // public static siteTitle: string = 'Conocimineto Ancestral';
->>>>>>> cc3e456a7faa8856466cc44c6ca05e54b096af98
 }
+
+
+
