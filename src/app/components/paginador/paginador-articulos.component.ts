70e2aef18637133e6dba396c9fe9816e45e27ce9
Daniel Josafat Garcia <yrinengan@gmail.com>
2020-06-19 00:17:43 -0500

    nuevos letreros

--- src/app/components/paginador/paginador-articulos.component.ts
+++ src/app/components/paginador/paginador-articulos.component.ts
@@ -18,27 +18,27 @@ export class PaginadorArticulosComponent implements OnInit {
 
   revistasResultado: [] = [];
   constructor(private Ruta: Router, private articulosService: ServiosBusquedaService,
-              private filtrosService: FiltrosService, private paginadorService: PaginadorService) { }
+    private filtrosService: FiltrosService, private paginadorService: PaginadorService) { }
 
-  ngOnInit(): void { 
-  //  console.log("##########################",this.articulosService.getpalabra())
+  ngOnInit(): void {
+    //  console.log("##########################",this.articulosService.getpalabra())
     //inicializa la busqueda con el primer metodo del servicio LeerJson 
     // this.revistasService.leerjson().subscribe((revistasDesdeApi: any) => {
     //   console.log(revistasDesdeApi.revistas.total);
-      // this.revistas = revistasDesdeApi.revistas.revistas;
-      // this.total.total = revistasDesdeApi.revistas.total;
-      // this.paginadorService.total = revistasDesdeApi.revistas.total;
-      // console.log('total', this.total.total);
-      // if (Number.isInteger((this.total.total / 12))) {
-      //   this.total.final = (this.total.total / 12)
-      // } else {
-      //   this.total.final = Math.floor(this.total.total / 12) + 1
-      // // }
-      // this.paginadorService.calculaFinal(revistasDesdeApi.revistas.total);
-      // this.paginadorService.posicion = 1;
-      // console.log(this.revistas)
-      // console.log(this.total)
-      // this.filtrosRevistasService.actualizarRevistas(revistasDesdeApi.revistas.revistas);
+    // this.revistas = revistasDesdeApi.revistas.revistas;
+    // this.total.total = revistasDesdeApi.revistas.total;
+    // this.paginadorService.total = revistasDesdeApi.revistas.total;
+    // console.log('total', this.total.total);
+    // if (Number.isInteger((this.total.total / 12))) {
+    //   this.total.final = (this.total.total / 12)
+    // } else {
+    //   this.total.final = Math.floor(this.total.total / 12) + 1
+    // // }
+    // this.paginadorService.calculaFinal(revistasDesdeApi.revistas.total);
+    // this.paginadorService.posicion = 1;
+    // console.log(this.revistas)
+    // console.log(this.total)
+    // this.filtrosRevistasService.actualizarRevistas(revistasDesdeApi.revistas.revistas);
     // });
     // this.total.palabra = this.revistasService.getpalabra();
     // this.filtrosRevistasService.cambioRevistas.subscribe(data2 => {
@@ -64,167 +64,70 @@ export class PaginadorArticulosComponent implements OnInit {
   }
 
   public ultimapagina(ultimapagina: number) {
-    // console.log(ultimapagina)
-    // let palabra = this.revistasService.palabra;
-    // console.log(palabra);
-    // this.revistasService.getPaginaFinal(palabra, ultimapagina).subscribe((data: any) => {
-    //   console.log(data);
-    //   this.revistas = data.revistas.revistas;
-    //   this.revistasService.setpalabra(palabra)
-    //   this.total.total = data.revistas.total;
-    //   if (Number.isInteger((this.total.total / 12))) {
-    //     this.total.final = (this.total.total / 12)
-    //     this.revistasService.setcount(this.total.final)
-    //   } else {
-    //     this.total.final = Math.floor(this.total.total / 12) + 1
-    //     this.revistasService.setcount(this.total.final)
-    //   }
-    //   this.total.pos = this.revistasService.count
-    //   this.revistasService.setfin(0)
-    // });
+
 
     this.articulosService.getBusquedaArticulosPaginador(this.articulosService.palabra, this.paginadorService.pFinal).
-    subscribe((data: any) =>{
-      console.log(data);
-      this.filtrosService.actualizarArticulos(data.articulos.articulos);
-      this.paginadorService.actualizarPosicion(this.paginadorService.pFinal);
-    });
+      subscribe((data: any) => {
+        console.log(data);
+        this.filtrosService.actualizarArticulos(data.articulos.articulos);
+        this.paginadorService.actualizarPosicion(this.paginadorService.pFinal);
+      });
   }
 
   public primerPagina() {
-    console.log("Palabra recibida en el boton de primer palabra",this.articulosService.getpalabra())
-    // let palabra = this.revistasService.getpalabra();
-    // console.log(palabra);
-    // this.revistasService.getPaginaP(palabra).subscribe((data: any) => {
-    //   console.log(data);
-    //   this.revistas = data.revistas.revistas;
-    //   this.revistasService.setpalabra(palabra)
-    //   this.total.total = data.revistas.total;
-    //   if (Number.isInteger((this.total.total / 12))) {
-    //     this.total.final = (this.total.total / 12)
-    //     this.revistasService.setcount(1)
-    //   } else {
-    //     this.total.final = Math.floor(this.total.total / 12) + 1
-    //     this.revistasService.setcount(1)
-    //   }
-    //   this.total.pos = this.revistasService.count
-    //   this.revistasService.setfin(1)
-    // });
-    this.articulosService.getBusquedaArticulosPaginador(this.articulosService.palabra, 1).subscribe((data: any) =>{
+    console.log("Palabra recibida en el boton de primer palabra", this.articulosService.getpalabra())
+
+    this.articulosService.getBusquedaArticulosPaginador(this.articulosService.palabra, 1).subscribe((data: any) => {
       this.filtrosService.actualizarArticulos(data.articulos.articulos);
       this.paginadorService.actualizarPosicion(1);
-          this.filtrosService.actualizarPalabra(this.articulosService.getpalabra());
+      this.filtrosService.actualizarPalabra(this.articulosService.getpalabra());
     });
 
   }
 
 
   public getCount() {
-    // console.log('pagina', this.paginadorService.posicion);
     return this.paginadorService.posicion;
   }
 
 
 
   public getFin() {
-    // this.total.pos = this.revistasService.count
-    // console.log('paginaFinal', this.paginadorService.pFinal);
     return this.paginadorService.pFinal;
   }
 
-  posicion(){
+  posicion() {
     return this.paginadorService.posicion;
   }
 
   public incCount() {
-    // let pagina = this.revistasService.getcount();
-    // let d = this.total.total; //11 
-    // let division = d % 12; // division = 1 
-    // if (division == 0 && pagina <= (d / 12)) {
-    //   pagina = pagina + 1
-    //   if (pagina <= (d / 12)) { //boton de pagina siguiente no sera mostrado llgando al final
-    //     this.revistasService.setfin(1)
-    //   } else {
-    //     this.revistasService.setfin(0)
-    //   } //boton de pagina siguiente no sera mostrado llgando al final
-    //   this.revistasService.setcount(pagina)
-    //   this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-    //     this.revistas = revistasDesdeApi.revistas.revistas
-    //     this.filtrosRevistasService.actualizarRevistas(revistasDesdeApi.revistas.revistas);
-    //   });
-    // } else {
-    //   if (!(Number.isInteger(d / 12)) && (pagina <= (d / 12))) {
-    //     pagina = pagina + 1
-    //     if (pagina <= (d / 12)) { //boton de pagina siguiente no sera mostrado llgando al final
-    //       this.revistasService.setfin(1)
-    //     } else {
-    //       this.revistasService.setfin(0)
-    //     }//boton de pagina siguiente no sera mostrado llgando al final
-    //     this.revistasService.setcount(pagina)
-    //     this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-    //       this.revistas = revistasDesdeApi.revistas.revistas
-    //       this.filtrosRevistasService.actualizarRevistas(revistasDesdeApi.revistas.revistas);
-    //     });
-    //   } else {
-    //     this.revistasService.setfin(0)
-    //   }
-    // }
-    // console.log(pagina)
-    // this.total.pos = this.revistasService.count
     console.log('siguiente');
     this.paginadorService.actualizarPosicion(this.paginadorService.posicion + 1);
     this.articulosService.getBusquedaArticulosPaginador(this.articulosService.palabra, this.paginadorService.posicion).
-    subscribe( (data: any) =>{
-      console.log('paginador', data);
-      this.filtrosService.actualizarArticulos(data.articulos.articulos);
-    });
+      subscribe((data: any) => {
+        console.log('paginador', data);
+        this.filtrosService.actualizarArticulos(data.articulos.articulos);
+      });
   }
 
 
-  // public getDCount() {
-  //   return this.revistasService.count
-  // }
   public incDCount() {
-    // this.revistasService.setfin(1)
-    // let pagina = this.revistasService.getcount();
-    // pagina = pagina - 1
-
-    // this.revistasService.setcount(pagina)
-    // this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-
-    //   this.revistas = revistasDesdeApi.revistas.revistas;
-
-    // });
-    // this.total.pos = this.revistasService.count
     console.log('anterior');
     this.paginadorService.actualizarPosicion(this.paginadorService.posicion - 1);
     this.articulosService.getBusquedaArticulosPaginador(this.articulosService.palabra, this.paginadorService.posicion).
-    subscribe( (data: any) =>{
-      console.log('paginador', data);
-      this.filtrosService.actualizarArticulos(data.articulos.articulos);
-    });
+      subscribe((data: any) => {
+        console.log('paginador', data);
+        this.filtrosService.actualizarArticulos(data.articulos.articulos);
+      });
   }
 
   public numerosPag(pagina: number, final: number) {
-   
-  //   if (pagina === final) {
-  //     this.revistasService.setfin(0)
-  //   } else {
-  //     this.revistasService.setfin(1)
-  //   }
-  //   this.revistasService.setcount(pagina)
-  //   this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-  //     this.revistas = revistasDesdeApi.revistas.revistas;
-  //   });
-  //   this.total.pos = this.revistasService.count
-  // }
+    this.paginadorService.actualizarPosicion(pagina);
+    this.filtrosService.actualizarPalabra(this.articulosService.getpalabra())
+    this.articulosService.getBusquedaArticulosPaginador(this.filtrosService.palabra, pagina).subscribe((data: any) => {
+      this.filtrosService.actualizarArticulos(data.articulos.articulos);
+    });
 
-  this.paginadorService.actualizarPosicion(pagina);
-  this.filtrosService.actualizarPalabra(this.articulosService.getpalabra())
-  this.articulosService.getBusquedaArticulosPaginador(this.filtrosService.palabra, pagina).subscribe((data: any) =>{
-    this.filtrosService.actualizarArticulos(data.articulos.articulos);   
-  });
-
-}
+  }
 
 }
--- src/app/components/revistas-table/revistas-table.component.ts
+++ src/app/components/revistas-table/revistas-table.component.ts
@@ -28,16 +28,10 @@ export class RevistasTableComponent implements OnInit {
     private paginadorService: PaginadorService, ) { }
 
 
-  ngOnInit(): void { //inicializa la busqueda con el primer metodo del servicio LeerJson 
+  ngOnInit(): void {
     this.revistasService.leerjson().subscribe((revistasDesdeApi: any) => {
-      console.log(revistasDesdeApi);
-      // this.revistas = revistasDesdeApi.revistas.revistas;
-      this.paginadorService.total = revistasDesdeApi.revistas.total;
-      // if (Number.isInteger((this.total.total / 12))) {
-      //   this.total.final = (this.total.total / 12)
-      // } else {
-      //   this.total.final = Math.floor(this.total.total / 12) + 1
-      // }
+      console.log(revistasDesdeApi);     
+      this.paginadorService.total = revistasDesdeApi.revistas.total;    
       this.paginadorService.actualizarTotal(revistasDesdeApi.revistas.total, 'revistas');
       this.paginadorService.actualizarPosicion(1);
       console.log('Pposiscion', this.paginadorService.posicion);
@@ -46,7 +40,6 @@ export class RevistasTableComponent implements OnInit {
       this.filtrosRevistasService.actualizarRevistas(revistasDesdeApi.revistas.revistas);
       this.filtrosRevistasService.actualizarFiltros(revistasDesdeApi.filtros);
     });
-    // this.total.palabra = this.revistasService.getpalabra();
     this.filtrosRevistasService.cambioRevistas.subscribe(data2 => {
       console.log('resutladosServicio', data2);
       this.revistas = data2;
@@ -54,8 +47,6 @@ export class RevistasTableComponent implements OnInit {
     this.total.palabra = this.filtrosRevistasService.palabra;
     this.total.total = this.paginadorService.total;
   }
-
-
 
   buscar(palabra: string) {
     console.log(palabra);
@@ -76,134 +67,5 @@ export class RevistasTableComponent implements OnInit {
     this.paginadorService.reversa = false;
     this.filtrosRevistasService.actualizarPalabra(palabra);
   }
-
-
-  // public posicionActual() {
-  //   let posicion = this.revistasService.count
-  //   console.log(posicion)
-  // }
-
-  // public ultimapagina(ultimapagina: number) {
-  //   console.log(ultimapagina)
-  //   let palabra = this.revistasService.getpalabra();
-  //   console.log(palabra);
-  //   this.revistasService.getPaginaFinal(palabra, ultimapagina).subscribe((data: any) => {
-  //     console.log(data);
-  //     this.revistas = data.revistas.revistas;
-  //     this.revistasService.setpalabra(palabra)
-  //     this.total.total = data.revistas.total;
-  //     if (Number.isInteger((this.total.total / 12))) {
-  //       this.total.final = (this.total.total / 12)
-  //       this.revistasService.setcount(this.total.final)
-  //     } else {
-  //       this.total.final = Math.floor(this.total.total / 12) + 1
-  //       this.revistasService.setcount(this.total.final)
-  //     }
-  //     this.total.pos = this.revistasService.count
-  //     this.revistasService.setfin(0)
-  //   });
-  // }
-
-  // public primerPagina() {
-  //   let palabra = this.revistasService.getpalabra();
-  //   console.log(palabra);
-  //   this.revistasService.getPaginaP(palabra).subscribe((data: any) => {
-  //     console.log(data);
-  //     this.revistas = data.revistas.revistas;
-  //     this.revistasService.setpalabra(palabra)
-  //     this.total.total = data.revistas.total;
-  //     if (Number.isInteger((this.total.total / 12))) {
-  //       this.total.final = (this.total.total / 12)
-  //       this.revistasService.setcount(1)
-  //     } else {
-  //       this.total.final = Math.floor(this.total.total / 12) + 1
-  //       this.revistasService.setcount(1)
-  //     }
-  //     this.total.pos = this.revistasService.count
-  //     this.revistasService.setfin(1)
-  //   });
-  // }
-
-
-  // public getCount() {
-  //   return this.revistasService.count
-  // }
-
-
-
-  // public getFin() {
-  //   this.total.pos = this.revistasService.count
-  //   return this.revistasService.fin
-  // }
-
-  // public incCount() {
-  //   let pagina = this.revistasService.getcount();
-  //   let d = this.total.total; //11 
-  //   let division = d % 12; // division = 1 
-  //   if (division == 0 && pagina <= (d / 12)) {
-  //     pagina = pagina + 1
-  //     if (pagina <= (d / 12)) { //boton de pagina siguiente no sera mostrado llgando al final
-  //       this.revistasService.setfin(1)
-  //     } else {
-  //       this.revistasService.setfin(0)
-  //     } //boton de pagina siguiente no sera mostrado llgando al final
-  //     this.revistasService.setcount(pagina)
-  //     this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-  //       this.revistas = revistasDesdeApi.revistas.revistas
-  //       this.filtrosRevistasService.actualizarRevistas(revistasDesdeApi.revistas.revistas);
-  //     });
-  //   } else {
-  //     if (!(Number.isInteger(d / 12)) && (pagina <= (d / 12))) {
-  //       pagina = pagina + 1
-  //       if (pagina <= (d / 12)) { //boton de pagina siguiente no sera mostrado llgando al final
-  //         this.revistasService.setfin(1)
-  //       } else {
-  //         this.revistasService.setfin(0)
-  //       }//boton de pagina siguiente no sera mostrado llgando al final
-  //       this.revistasService.setcount(pagina)
-  //       this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-  //         this.revistas = revistasDesdeApi.revistas.revistas
-  //         this.filtrosRevistasService.actualizarRevistas(revistasDesdeApi.revistas.revistas);
-  //       });
-  //     } else {
-  //       this.revistasService.setfin(0)
-  //     }
-  //   }
-  //   console.log(pagina)
-  //   this.total.pos = this.revistasService.count
-  // }
-
-
-  // public getDCount() {
-  //   return this.revistasService.count
-  // }
-  // public incDCount() {
-  //   this.revistasService.setfin(1)
-  //   let pagina = this.revistasService.getcount();
-  //   pagina = pagina - 1
-
-  //   this.revistasService.setcount(pagina)
-  //   this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-
-  //     this.revistas = revistasDesdeApi.revistas.revistas;
-
-  //   });
-  //   this.total.pos = this.revistasService.count
-  // }
-
-  // public numerosPag(pagina: number, final: number) {
-
-  //   if (pagina == final) {
-  //     this.revistasService.setfin(0)
-  //   } else {
-  //     this.revistasService.setfin(1)
-  //   }
-  //   this.revistasService.setcount(pagina)
-  //   this.RevistasInyectado.leerjson().subscribe((revistasDesdeApi: any) => {
-  //     this.revistas = revistasDesdeApi.revistas.revistas;
-  //   });
-  //   this.total.pos = this.revistasService.count
-  // }
-
 
 }
