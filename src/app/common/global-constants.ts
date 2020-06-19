ab650f0d0968111cc35ed94d23d7fce8ff704311
Mario Flores <floresmario96@hotmail.com>
2020-06-19 17:17:07 -0500

    Acerca de listo sin responsivo

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -8,5 +8,3 @@ export class GlobalConstants {
     public static url: string = 'http://amelica.org/ConocimientoAncestral/';
     public static siteTitle: string = 'Conocimineto Ancestral';
 }
-
-//13,308.
