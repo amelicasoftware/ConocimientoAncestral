c66c0f87fa06d8c145f5c32704626a627856b47c
Luis Angel Angeles <angel_apineda@hotmail.com>
2020-07-02 18:39:52 -0500

    correcciones final

--- src/assets/js/mapaHome.js
+++ src/assets/js/mapaHome.js
@@ -1,8 +1,8 @@
 am4core.ready(function() {
 
-	// let url = "http://localhost:4200/";
-	// let url = "http://localhost:8080/";
-	let url = "http://amelica.org/ConocimientoAncestral/";
+	let url = "http://localhost:4200/";
+	// let url = "http://semantic.redalyc.org/";
+	// let url = "http://amelica.org/ConocimientoAncestral/";
 	console.log('voy hacer la graficas44444');
 	am4core.useTheme(am4themes_animated);
 	// Themes end
