2fb119c1668ce7d3a85b4887b46f40c109ba7d68
Mario Flores <floresmario96@hotmail.com>
2020-10-05 18:02:14 -0500

    Generar dist solucion del problema de nombre de campo

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,8 +1,8 @@
 export class GlobalConstants {
 
     //public static url: string = 'http://localhost:8080/';
-    public static serviciosURL: string = 'http://localhost:8080/';
-     public static url: string = 'http://localhost:4200/';
+    //public static serviciosURL: string = 'http://localhost:8080/';
+    //public static url: string = 'http://localhost:4200/';
     //produccion
     //public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/';
     //public static url: string = 'http://amelica.org/ConocimientoAncestral/';
